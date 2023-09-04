2bc58133b8dca17b36cfdef5e18bcbcb97ca1147
Wilma Rudolfsson <wilma.rudolfsson06@skola.stenungsund.se>
2023-09-04 10:40:39 +0200

    first

---
+++ 7040 in- och utmatning/7040 in- och utmatning.py
@@ -0,0 +1,52 @@
+'''
+namn=input("Vad heter du?")
+print("Hej", namn, "!")
+'''
+'''
+namn=input("Vad heter du?")
+färg=input("Vad är din favoritfärg?")
+print(namn, "din favoritfärg är",färg,)
+'''
+'''
+namn=input("Vad heter du?")
+print(namn,"är ett fint namn")
+favoritmat=input("Vad är din favoritmat?")
+print(namn,favoritmat,"är verkligen gott")
+pengar=input("Hur mycket pengar har du i fickan?")
+print(namn,"du är rik")
+'''
+'''
+print("Ett",end="&")
+print("två")
+'''
+'''
+bredd=float(input("Ange rektangelns bredd:"))
+höjd=float(input("Ange rektangelns höjd:"))
+area=bredd*höjd
+omkrets=2*(bredd+höjd)
+print("area:",area)
+print("omkrets:",omkrets)
+'''
+'''
+bas=float(input("Ange triangelns bas:"))
+höjd=float(input("Ange triangelns höjd:"))
+area=bas*höjd/2
+print("bas=",bas)
+print("höjd=",höjd)
+print("area=",area)
+'''
+'''
+r=float(input("Ange cirkelns radie:"))
+area=r*r*3.14
+omkrets=2*r*3.14
+print("area:",area)
+print("omkrets:",omkrets)
+'''
+
+
+
+                
+
+      
+      
+
