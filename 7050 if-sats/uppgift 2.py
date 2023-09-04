76a9dd50c42027239047b400c915b22459223cec
Wilma Rudolfsson <wilma.rudolfsson06@skola.stenungsund.se>
2023-09-04 11:10:46 +0200

    inte klar

---
+++ 7050 if-sats/uppgift 2.py
@@ -0,0 +1,15 @@
+tal=int(input("Mata in din näst sista siffra i ditt personnummer"))
+if tal:
+    #sats 0
+    #sats 2
+    #sats 4
+    #sats 6
+    #sats 8
+    print("du är en tjej")
+else tal:
+    #sats 1
+    #sats 3
+    #sats 5
+    #sats 7
+    #sats 9
+    print("du är en kille")
