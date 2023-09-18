3e1e54381854440484b5ffddf4338c67207c4839
Wilma Rudolfsson <wilma.rudolfsson06@skola.stenungsund.se>
2023-09-18 11:08:21 +0200

    inte klar

---
+++ 7070 slumptal/uppgift 1.py
@@ -0,0 +1,10 @@
+tal=(input("Vill du spela? ja/nej" ))
+if ("ja"):
+    import random
+#[1, 6)
+print (random.randrange(1, 6)) 
+print (random.randrange(1, 6))
+if random.randrange == random.randrange:
+    print ("förlust")
+else :("nej")
+print ("Vad roligt att du spelade en stund!")
