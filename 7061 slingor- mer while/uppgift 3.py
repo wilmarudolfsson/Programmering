dcf4641a0279c08a539e92080339e91089bb9cf9
Wilma Rudolfsson <wilma.rudolfsson06@skola.stenungsund.se>
2023-09-18 10:35:55 +0200

    klar

--- 7061 slingor- mer while/uppgift 3.py
+++ 7061 slingor- mer while/uppgift 3.py
@@ -1,6 +1,6 @@
 svar=int(input("Gissa på ett tal:"))
 antal_gissningar = 1
-while svar !=42 and antal_gissningar < 5:
+while svar !=42 and antal_gissningar < 3:
     if svar < 42:
         print("För litet, Gissa igen")
     else:
@@ -8,4 +8,3 @@ while svar !=42 and antal_gissningar < 5:
     svar=int(input("Gissa på ett tal:"))
     antal_gissningar = antal_gissningar + 1
 print("slut")
-        
