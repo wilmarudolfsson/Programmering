bf1c8f38598b3e40dc80c8ff6562c1e867d836ab
Wilma Rudolfsson <wilma.rudolfsson06@skola.stenungsund.se>
2023-09-24 20:22:53 +0200

    inte klar

--- 7070 slumptal/uppgift D.py
+++ 7070 slumptal/uppgift D.py
@@ -7,9 +7,21 @@ print("två lika - 5 kr")
 print("en sexa - 3 kr")
 print("stege - 3 kr")
 
-svar= input ("Välj spela (s), sätt in pengar (i), eller avsluta (a):")
-if svar== "i":
-    input("Ange belopp att sätta in: ")
-svar_2=svar
-if svar_2:
-    print("Att spela för:",svar_2)
+svar = input ("Välj spela (s), sätt in pengar (i), eller avsluta (a):")
+if svar == "i":
+    svar= input("Ange belopp att sätta in: ")
+    print("Att spela för:", svar)
+svar_2 = input ("Välj spela (s), sätt in pengar (i), eller avsluta (a):")
+while svar_2 == "s":
+    #[1, 6)
+    tal_1 = random.randrange (1, 6)
+    tal_2 = random.randrange (1, 6)
+    if tal_1 == tal_2:
+        print(" två lika-vinst + 5kr")
+        input("Att spela för: ",svar + 5)
+    else:
+        print("förlust")
+        input("Att spela för", svar--1)
+    
+
+
