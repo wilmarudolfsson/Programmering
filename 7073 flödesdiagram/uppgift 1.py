a4742f84542284ea4f1da69aa914715651eac383
Wilma Rudolfsson <wilma.rudolfsson06@skola.stenungsund.se>
2023-10-09 11:10:32 +0200

    klar

---
+++ 7073 flödesdiagram/uppgift 1.py
@@ -0,0 +1,7 @@
+svar= input ("Gissa på ett tal: ")
+while svar != "42":
+    svar= input("Du gissade fel. Gissa på ett annat tal. ")
+else:
+    print ("Du gissade rätt!")
+    print ("klar")
+    
---
+++ 7073 flödesdiagram/uppgift 2.py
@@ -0,0 +1,5 @@
+index = 0
+while index < 5:
+    print (index)
+    index = index + 1
+print ("klar")
