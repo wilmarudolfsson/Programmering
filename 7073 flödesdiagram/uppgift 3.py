785f2ff58f496247ea05c401fbc75a0c0948f4d1
Wilma Rudolfsson <wilma.rudolfsson06@skola.stenungsund.se>
2023-10-13 16:04:00 +0200

    klar

---
+++ 7073 flödesdiagram/uppgift 3.py
@@ -0,0 +1,9 @@
+text_1 = input("Skriv in ett tal")
+tal_1 = int(text_1)
+text_2 = input("Skriv in ett annat tal")
+tal_2 = int(text_2)
+if tal_1 > tal_2:
+    print (tal_1, "är större")
+else:
+    print (tal_2, "är större")
+    
