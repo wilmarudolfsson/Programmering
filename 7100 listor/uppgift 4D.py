4d324f419ac37e88a0e1d7d904d1e2a02e87ef51
Wilma Rudolfsson <wilma.rudolfsson06@skola.stenungsund.se>
2023-12-11 12:24:56 +0100

    håller på och testar

--- 7100 listor/uppgift 4D.py
+++ 7100 listor/uppgift 4D.py
@@ -1,4 +1,4 @@
-text = input("Vill du kryptera eller dekryptera? Skriv k eller d: ")
+'''text = input("Vill du kryptera eller dekryptera? Skriv k eller d: ")
 text_2 = input("Skriv in ditt ord: ")
 text_steg = input("Skifta antal steg: ")
 
@@ -25,4 +25,38 @@ if text == 'd':
             i = i + 29
         meddelande += svar[i]
 
-    print(meddelande)
+    print(meddelande)'''
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
